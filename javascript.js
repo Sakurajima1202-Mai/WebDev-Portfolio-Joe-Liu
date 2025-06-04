87da08c7c404d8c30015d7850fa8531fa1095b0a
Sakurajima1202-Mai <1278736918@qq.com>
2025-06-04 19:55:35 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -8,10 +8,28 @@
     });
   
 
-  function highlight(element) {
-    element.style.color = "orange";
+
+/*Define the button which can move to the top*/
+  window.onscroll = function() {
+    const btn = document.getElementById("backToTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+  
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
 
-  function removeHighlight(element) {
-    element.style.color = "white";
-  }
+
+  /*Describe the feedback part*/
+  document.getElementById('feedback-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('feedback-form').reset();
+    document.getElementById('feedback-thankyou').style.display = 'block';
+  });
