af595161e9f9bdb1a16955bccfd3aa4fd707997c
Sakurajima1202-Mai <1278736918@qq.com>
2025-06-04 20:40:45 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -10,21 +10,12 @@
 
 
 /*Define the button which can move to the top*/
-  window.onscroll = function() {
-    const btn = document.getElementById("backToTopBtn");
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  };
-  
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'  
+  });
+}
 
 
   /*Describe the feedback part*/
