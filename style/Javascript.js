121218fa20dbfdf2616bb0a421a74ce7e8c03894
Sakurajima1202-Mai <1278736918@qq.com>
2025-06-04 09:47:29 +0800

    Delete style directory

--- style/Javascript.js
+++
@@ -1,9 +0,0 @@
-
-    const design = document.querySelectorAll('.design');
-    design.forEach(q => {
-      q.addEventListener('click', () => {
-        const answer = q.nextElementSibling;
-        answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
-      });
-    });
-  
